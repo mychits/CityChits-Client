d18eb86edb521f032b8a1d495f4dc46f4f54ace6
jeev-creator <aruvajeevitha@gmail.com>
2025-10-31 11:05:16 +0530

    instance updated to vercel

--- src/instance/TokenInstance.js
+++ src/instance/TokenInstance.js
@@ -6,7 +6,9 @@ const api = axios.create({
 
   	// baseURL: "http://localhost:3000/api", 
 
-      baseURL: "https://citychit-prod-node.onrender.com/api",
+      // baseURL: "https://citychit-prod-node.onrender.com/api",
+
+      baseURL : "https://citychit-prod-node-pat4-git-main-mychits-projects.vercel.app/api"
 });
 
 
